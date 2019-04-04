76016e5133cae97a808fb839cd4d0032061bd7bb
Md. Al-Amin <41773444+alamin22@users.noreply.github.com>
2019-04-05 00:30:50 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>project with sass</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="main_body">
+<div class="header">
+	<div class="banner">
+		<h2>welcome to sass project page</h2>
+	</div>
+	<div class="menu">
+		<ul>
+			<li><a href="#">HOME</a></li>
+			<li><a href="#">ABOUT</a></li>
+			<li><a href="#">CONTACT</a></li>
+		</ul>
+	</div>
+</div>
+
+<div class="main_container">
+		<div class="container">
+			<h2>about us:</h2>
+			<p>bangladesh is small country.it has lot of beauty such as river,environment,population,forest.
+               bangladesh is small country.it has lot of beauty such as river,environment,population,forest.
+               bangladesh is small country.it has lot of beauty such as river,environment,population,forest.
+               bangladesh is small country.it has lot of beauty such as river,environment,population,forest.
+               bangladesh is small country.it has lot of beauty such as river,environment,population,forest.
+			</p>
+		</div>
+	</div>
+<div class="post">
+	<a href="#">open an account</a>
+</div>
+
+<div class="post2">
+	<a href="#">edit option for user</a>
+</div>
+<div class="post4">
+	<a href="#">delete your post</a>
+</div>
+	<div class="main_content">
+		<div class="content">
+		   <marquee behavior="alternate" >
+			<img src="image/images.jpg">
+			<img src="image/images2.jpg">
+           </marquee>
+		</div>
+	</div>
+
+	<div class="footer">
+		<h4>footer in our sass site</h4>
+		<div class="circle">
+			<p>sass</p>
+		</div>
+		<div class="circle2">
+			<p>sass</p>
+		</div>
+
+	</div>
+	</div>
+</body>
+</html>
